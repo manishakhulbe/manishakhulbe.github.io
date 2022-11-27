cc5964749e3ab714ba74da50393287bb9ce8ed97
Andres Sulg <aaendeereees@gmail.com>
2022-11-27 12:10:26 +0200

    class overview, tab highlighting
    - class overview graph with info on hover
    - fix highlighting active tab
    - fix displaying default tab on page load

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,48 @@
-function openTab(active_tab) {
+// TAB SWITCH FUNCTIONALITY
+function openTab(activeTabLink, activeTab) {
   const tabs = document.getElementsByClassName('tabcontent');
   for (const tab of tabs) {
-    console.log(tab, active_tab);
-    tab.style.display = tab.id == active_tab ? 'block' : 'none';
+    tab.style.display = tab.id == activeTab ? 'block' : 'none';
+  }
+  const tabLinks = document.getElementsByClassName('tablinks');
+  for (const tabLink of tabLinks) {
+    tabLink.className = tabLink.className.replace(' active', '');
+    if (tabLink == activeTabLink) {
+      tabLink.className += ' active';
+    }
   }
 }
-
 window.openTab = openTab;
+
+// DATA
+import studentsData from '../data/digitund_6.json' assert {type: 'json'};
+
+window.onload = () => {
+  // Display default tab
+  document.getElementById('ClassOverview').style.display = 'block';
+  const graphOverview = document.getElementById('graph-overview');
+  const students = studentsData.students;
+  students.sort((a, b) => a.performance - b.performance);
+  students.forEach((student) => {
+    const studentCell = document.createElement('div');
+    studentCell.classList.add('graph-overview-cell');
+    studentCell.classList.add('hovertext');
+    studentCell.setAttribute(
+      'data-hover',
+      `${student.name}: ${student.performance}`
+    );
+    switch (student.performance) {
+      case 5:
+      case 6:
+        studentCell.style.backgroundColor = 'rgb(102, 204, 255)';
+        break;
+      case 3:
+      case 4:
+        studentCell.style.backgroundColor = 'rgb(255, 204, 102)';
+        break;
+      default:
+        studentCell.style.backgroundColor = 'rgb(255, 83, 26)';
+    }
+    graphOverview.appendChild(studentCell);
+  });
+};
